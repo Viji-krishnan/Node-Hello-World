3b8713baf3974091d5b1f24ee824c2582570a52b
viji radhakrishnan <vijisweetie21@gmail.com>
2024-03-15 11:30:31 +0100

    Creating Hello world Web Application using Node.js

---
+++ index.js
@@ -0,0 +1,27 @@
+const fs = require('fs'); 
+const http = require('http');
+const url = require('url');
+
+const server = http.createServer((req, res) => {
+    const pathName = req.url;
+
+    if (pathName === '/' || pathName === '/overview') {
+        res.writeHead(200, {'content-type':'text/html'})
+        res.end('<h1>Hello World !!! This is the OVERVIEW</h1>');
+    } else if (pathName === '/product') {
+        res.end('This is the PRODUCT');
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'tele-404'
+        });
+        res.end( '<h1>Page not found !</h1>');
+    }
+    //res.end('Hello from the server!');
+});
+
+server.listen(8000, 'localhost');
+
+//fs.readFileSync()
+//const hello = 'Hello World';
+//console.log(hello);
